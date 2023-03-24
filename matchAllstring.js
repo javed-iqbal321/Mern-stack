6909571f0f27b43da76f811131f2b2fd5221d8fb
javed-iqbal <javedbalti1403@gmail.com>
2023-03-25 00:01:46 +0500

    matchAllstring.js

--- matchAllstring.js
+++ matchAllstring.js
@@ -1,3 +1,3 @@
-let text = "I love cats. Cats are very easy to love. Cats are very popular."
-let x = text.matchAll("/Cats/g");
-console.log(x);
+import message from " ./modulepath.js";
+
+console.log(message);
