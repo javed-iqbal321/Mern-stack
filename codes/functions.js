4ea913d72accfe9ea5390f53811719a5c7286d69
javed-iqbal <javedbalti1403@gmail.com>
2023-03-29 20:27:23 +0500

    previous files

---
+++ codes/functions.js
@@ -0,0 +1,51 @@
+// Function to print trainees' names
+function printTrainees(names) {
+  console.log("Trainees: ");
+  // for (let i = 0; i < names.length; i++) {
+  //   console.log(names[i]);
+  // }
+  names.map(name => {
+    console.log(name);
+    
+  });
+}
+
+// Function to check if a name is in the trainees list
+function isTrainee(name, names) {
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === name) {
+      return console.log(name + " is already in the trainees list.");
+    }
+  }
+  return console.log(name + " is not in the trainees list.");
+}
+
+// Function to add a new trainee
+function addTrainee(name, names) {
+  if (names.includes(name)){
+    return console.log(name + "name is already exist")
+  }
+  names.unshift(name);
+  return console.log(name + " added to the trainees list.");
+}
+
+// Example usage
+let traineesList = [
+  "aliya",
+  "malika",
+  "mehmooda",
+  "mudeera",
+  "shahina",
+  "fatima",
+  "qamar",
+  "akbar",
+  "zakir",
+  "shehbaz",
+  "javed",
+  "tehzeeb",
+];
+
+
+isTrainee("iqbal", traineesList);
+addTrainee("iqbal", traineesList);
+printTrainees(traineesList);
